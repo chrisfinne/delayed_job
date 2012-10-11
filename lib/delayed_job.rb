a3bc94f93cd7ed604e8aa54332a4aed2c7df6f94
chrisfinne <chris.finne@gmail.com>
2012-10-10 18:16:15 -0700

    version fix

--- lib/delayed_job.rb
+++ lib/delayed_job.rb
@@ -5,7 +5,7 @@ require 'delayed/performable_method'
 # PerformableMailer is compatible with ActionMailer 3 (and possibly 3.1)
 if defined?(ActionMailer)
   require 'action_mailer/version'
-  require 'delayed/performable_mailer' if 3 == ActionMailer::VERSION::MAJOR
+  require 'delayed/performable_mailer' if 4 == ActionMailer::VERSION::MAJOR
 end
 
 require 'delayed/yaml_ext'
