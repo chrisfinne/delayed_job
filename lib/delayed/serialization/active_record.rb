f2e2d8a5bfab570747d02e3c344d27d1b42cfcac
chris finne <chris.finne@gmail.com>
2012-10-12 20:20:51 -0700

    Rails 4

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -3,7 +3,7 @@ if defined?(ActiveRecord)
     yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
 
     def self.yaml_new(klass, tag, val)
-      if ActiveRecord::VERSION::MAJOR == 3
+      if ActiveRecord::VERSION::MAJOR >= 3
         klass.unscoped.find(val['attributes'][klass.primary_key])
       else # Rails 2
         klass.with_exclusive_scope { klass.find(val['attributes'][klass.primary_key]) }
